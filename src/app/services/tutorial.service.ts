7cfa27ee9f3ef11f0432efe735228aaa8952178c
Akshit Dayal <akshitdayal99@gmail.com>
2023-03-11 14:34:37 -0500

    Finished tutorial

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Tutorial } from '../models/tutorial.model';
 
+// Local API URL
 const baseUrl = "http://localhost:8000/api/tutorials";
 
 @Injectable({
